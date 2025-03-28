85c314506d9c6dacaca4020f72e97d4743d51276
KIEU Thi phuong <thi-phuong.kieu@lis-lab.fr>
2025-03-28 12:22:49 +0100

    20km

---
+++ 20km/IDM-model/runLargeScale.py
@@ -0,0 +1,46 @@
+import traci
+from sumolib import checkBinary
+
+sumoBinary = checkBinary('sumo')
+
+def main():
+
+    runSimulation(100000,1)
+    runSimulation(200000,1)
+    runSimulation(300000,1)
+    runSimulation(400000,1)
+    runSimulation(500000,1)
+    runSimulation(1000000,1)
+
+def runSimulation(nbCars, nbFile):
+    # Start simulation 
+    traci.start([
+        'sumo',
+        '-n', './config/net1000.net.xml',
+        '-r', f'./input/trips{nbCars}_{nbFile}.rou.xml',
+        '--log', f'./output/log{nbCars}_{nbFile}.txt',
+        # '--netstate-dump', f'./output/dump{nbCars}_{nbFile}.out.xml',
+        # '--summary', f'./output/summary{nbCars}_{nbFile}.xml',
+        '--start'
+    ])
+
+    # Move simulation step
+    time = 0
+        while time < 86399:
+            traci.simulationStep()
+            time+=1
+
+    # End simulation
+    traci.close()
+
+    print(f"{nbCars}_{nbFile} done")
+
+def shouldContinueSim():
+    # Checks that the simulation should continue running
+    # Returns: (bool) 'True' if vehicles exist on network, otherwise 'False'
+
+    numVehicles = traci.simulation.getMinExpectedNumber()
+    return True if numVehicles > 0 else False
+
+if __name__ == "__main__":
+    main()
